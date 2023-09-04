e6e528d31bea1e001c4a0094187158a0a270cb58
eamedranoc <eamedranoc@gmail.com>
2023-09-03 20:50:48 -0500

    Agregar archivos de proyecto.

---
+++ comunicacionRed/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace comunicacionRed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest peticion = WebRequest.Create("http://www.mercadolibre.com.co");
+            WebResponse respuesta = peticion.GetResponse();
+            StreamReader  respuestaCadena = new StreamReader(respuesta.GetResponseStream());
+            string responseText = respuestaCadena.ReadToEnd();
+            Console.WriteLine(responseText);
+            respuestaCadena.Close();
+            Console.ReadKey();
+        }
+    }
+}
